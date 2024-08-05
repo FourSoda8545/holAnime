9d198152fc24df43c5d8f091efb43de5e156eded
FourSoda8545 <aldost939@gmail.com>
2024-08-05 00:02:17 -0600

    versión 1.1 | base de app

--- animeApp.py
+++ animeApp.py
@@ -1,9 +1,9 @@
-## app para ver animes 
-
 from animefvl import AnimeFVL
+
 with AnimeFVL() as api:
-    anime = api.search_anime("FUll Metal Alchemist")
-    elements = api.search(input("Escribe la serie: "))
+    anime_name = input("Escribe el nombre del anime: ")
+    anime = api.search_anime(anime_name)
+    elements = api.search(anime_name)
 
     for i, element in enumerate(elements):
         print(f"{i}, {element.title}")
@@ -13,16 +13,16 @@ with AnimeFVL() as api:
         print(f"{i}, {element.score}")
         
         try:
-            selection = int(input("select option:"))
+            selection = int(input("Selecciona una opción: "))
             info = api.get_anime_info(elements[selection].id)
             info.episodes.reverse()
             
             for j, episode in enumerate(info.episodes):
                 print(f"{j} | Episode - {episode.id}")
-            index_episode = int(input("Select episode"))
+            index_episode = int(input("Selecciona el episodio: "))
             serie = elements[selection].id
             capitulo = info.episodes[index_episode].id
-            results = api.get_links(Serie, capitulo)
+            results = api.get_links(serie, capitulo)
             for result in results:
                 print(f"{result.server} - ********")
                 
